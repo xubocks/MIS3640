49db05c595b645029f0228a229561736f4d6674c
xubocks <xlu1@babson.edu>
2017-09-29 23:56:29 -0400

    Add files via upload

---
+++ session 8 in class.py
@@ -0,0 +1,16 @@
+def value(letter):
+    return ord(letter) - ord('a') + 1
+
+#print (value("a"))
+
+def price(word, letter):
+    price = 0
+    for i in word
+        if i == letter
+            price = price + value(letter)
+    reuturn price
+
+print(price('bananas'))
+print(price('rice'))
+print(price('paprika'))
+print(price('potato chips'))
