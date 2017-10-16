c44fa2ce2d823651ccfb0d83285f5571638f4b5d
xubocks <xlu1@babson.edu>
2017-10-15 22:50:44 -0400

    Add files via upload

---
+++ session11_inclassactivity.py
@@ -0,0 +1,25 @@
+fin = open('words.txt')
+
+
+
+
+def dictionary(word):
+    word = open('words.txt')
+    d = dict()
+    for line in word:
+        if dict(word) in word
+    return word
+
+
+
+
+def has_duplicates:
+    d = dict()
+    for s in list:
+        d[s] = 1 + d.get(s, 0)
+        if d[s] > 1:
+            return True
+        else:
+            return False
+
+list = ['chicken', 'chicken', 'tenders', 'buffalo', 'chicken']
